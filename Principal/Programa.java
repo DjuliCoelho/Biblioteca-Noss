9d3a4e75824fe38f7a14f4a1b9c40c8dcdbbaea9
Djuli Coelho <126629987+DjuliCoelho@users.noreply.github.com>
2023-06-08 20:15:13 -0300

    Update Programa.java
    ex programa

--- Principal/Programa.java
+++ Principal/Programa.java
@@ -10,88 +10,46 @@ import java.util.ArrayList;
 import Principal.model.*;
 import Principal.view.*;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Programa{
 
-    public static void main(String[] args) throws FileNotFoundException {
-    	
-    	Catalogo catalogo = new Catalogo();
-    	
-    	List<Cliente> listaClientes = new ArrayList<>();
+	public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("ex_mysql");
+        EntityManager em = emf.createEntityManager();
 
-        //leitura de arquivos
-    	
-        String arquivoOrigemLivros = "C:\\Users\\djuli\\OneDrive\\Documentos\\gravaArq\\BibliotecaDoLeo\\livros.csv";
-		File arquivoLivros = new File(arquivoOrigemLivros);
-		Scanner leitorLivros = new Scanner(arquivoLivros);
-		
-		String arquivoOrigemClientes = "C:\\Users\\djuli\\OneDrive\\Documentos\\gravaArq\\BibliotecaDoLeo\\clientes.csv";
-		File arquivoCliente = new File(arquivoOrigemClientes);
-		Scanner leitorClientes = new Scanner(arquivoCliente);
+        Catalogo catalogo = new Catalogo();
+        List<Cliente> listaClientes = new ArrayList<>();
 
-		String headerLivros = leitorLivros.nextLine();
-		String headerClientes = leitorClientes.nextLine();
-
-        while(leitorLivros.hasNextLine()) {
-        	String linha = leitorLivros.nextLine();
-        	String[] valores = linha.split(";");
-        	
-        	catalogo.addLivro(new Livro(Integer.parseInt(valores[0]), valores[1], Integer.parseInt(valores[2]), Integer.parseInt(valores[3])));
+        // Leitura de entidades do banco de dados
+        List<Livro> livros = em.createQuery("SELECT l FROM Livro l", Livro.class).getResultList();
+        for (Livro livro : livros) {
+            catalogo.addLivro(livro);
         }
-        
-        
-        while(leitorClientes.hasNextLine()) {
-        	String linha = leitorClientes.nextLine();
-        	String[] valores = linha.split(";");
-        	
-        	listaClientes.add(new Cliente(valores[0], Integer.parseInt(valores[1]), Integer.parseInt(valores[2])));
-        	
-        }
-		
-        
-        //menu e execução
-        
+
+        List<Cliente> clientes = em.createQuery("SELECT c FROM Cliente c", Cliente.class).getResultList();
+        listaClientes.addAll(clientes);
+
+        // Menu e execução
         Menu menu = new Menu(catalogo, listaClientes);
-        
         menu.mostrarMenu();
-        	
-        
-        
 
-        
-        //gravação de arquivos
+        // Gravação de entidades no banco de dados
+        em.getTransaction().begin();
+        for (Livro livro : catalogo.getLivros()) {
+            em.persist(livro);
+        }
+        for (Cliente cliente : listaClientes) {
+            em.persist(cliente);
+        }
+        em.getTransaction().commit();
 
-        String arquivoDestinoLivros = "C:\\Users\\djuli\\OneDrive\\Documentos\\gravaArq\\BibliotecaDoLeo\\livros.csv";
-		Formatter gravador = new Formatter(arquivoDestinoLivros);
-		
-		gravador.format(headerLivros + "\n");
-		for(int i=0; i<catalogo.ultimo(); i++) {
-			gravador.format(catalogo.getLivro(i).getId() + ";" + catalogo.getLivro(i).getNome() + ";" + catalogo.getLivro(i).getQuantidade() + ";" + catalogo.getLivro(i).getQteretirada() + ";\n");
-		}
-		
-		String arquivoDestinoClientes = "C:\\Users\\djuli\\OneDrive\\Documentos\\gravaArq\\BibliotecaDoLeo\\clientes.csv";
-		Formatter gravadorClientes = new Formatter(arquivoDestinoClientes);
-		
-		gravadorClientes.format(headerClientes + "\n");
-		
-		for(Cliente c : listaClientes) {
-			gravadorClientes.format(c.getNome() + ";" + c.getEspera() + ";" + c.getRetirado() + ";\n");
-		}
-       
-		leitorClientes.close();
-        leitorLivros.close();
-        gravador.close();
-        gravadorClientes.close();
-        
-        
-            
-        
-        
-
+        em.close();
+        emf.close();
     }
 }
-		        
-		        
-
 		    
 		
 	    
